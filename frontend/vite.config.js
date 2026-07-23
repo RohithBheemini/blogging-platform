3a24dd69a0d9960b2e6378e0c5966be65a22fcb4
Bheemini Venkat Rohith <rohithbheemini25@gmail.com>
2026-07-23 22:38:55 +0530

    Update vite.config.js
    Vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,11 +16,5 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
-    rollupOptions: {
-      output: {
-        
-        },
-      },
-    },
   },
 })
